ad2319c5032af7d381a34fa16c7e53ba8d01838b
bryan <bryanmills86@yahoo.com>
2018-05-11 20:01:15 -0700

    Commit API key to main branch because of accidental deletion :O

--- tv/src/main/java/com/californiadreamshostel/officetv/MODELS/Weather/SSSuperSecretAPIKey.java
+++ tv/src/main/java/com/californiadreamshostel/officetv/MODELS/Weather/SSSuperSecretAPIKey.java
@@ -1,6 +1,7 @@
 package com.californiadreamshostel.officetv.MODELS.Weather;
 
 public class SSSuperSecretAPIKey {
+    public static final String API_KEY_WEATHER_OWM = "83f6b3e442f8632912d893ff2e23ae98";
     public static final String API_KEY_WEATHER_DARK_SKY = "765857743e8e4f877c7f37d4770ae024";
 
 }
