8c6c8ded338ab3b126cc0566dc5650a0cea7b45f
bryan <bryanmills86@yahoo.com>
2018-06-01 20:35:44 -0700

    Commit core API for the Unit Conversions

--- tv/src/main/java/com/californiadreamshostel/officetv/UNIT/UnitCache.java
+++ tv/src/main/java/com/californiadreamshostel/officetv/UNIT/UnitCache.java
@@ -1,6 +1,7 @@
 package com.californiadreamshostel.officetv.UNIT;
 
 import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.californiadreamshostel.officetv.UNIT.UNITS.Unit;
 
@@ -9,9 +10,12 @@ public class UnitCache {
     private int id;
     private Unit unit;
 
-    public void update(final int id,@NonNull final Unit unit){
+    public void update(final int id, @NonNull final Unit unit){
         this.id = id;
         this.unit = unit;
+
+        Log.i("UNIT_FIX", "UnitCache#update() The Unit type is: " + unit.getType());
+
     }
 
     public int getId(){
--- tv/src/main/java/com/californiadreamshostel/officetv/UNIT/UnitChoreographer.java
+++ tv/src/main/java/com/californiadreamshostel/officetv/UNIT/UnitChoreographer.java
@@ -2,6 +2,7 @@ package com.californiadreamshostel.officetv.UNIT;
 
 import android.animation.Animator;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.californiadreamshostel.officetv.UNIT.CONVERTER.UnitConvertorFactory;
@@ -9,29 +10,37 @@ import com.californiadreamshostel.officetv.UNIT.UNITS.Unit;
 import com.californiadreamshostel.officetv.UTILITIES.ANIMATIONS.OnAnimationEnd;
 import com.californiadreamshostel.officetv.UTILITIES.ANIMATIONS.TextAnimator;
 
+
+//TODO Refractor a views id out of the UnitCache mapping.
 public class UnitChoreographer {
+
+    public static final String TAG = "UnitChoreographer";
 
     private TextView subject;
 
     private TextAnimator animator;
 
-    private UnitCache unitTracker;
+    private UnitCache unitCache;
 
     private Unit tempTo;
 
-    public UnitChoreographer(){
-        this.unitTracker = new UnitCache();
+    public UnitChoreographer(@NonNull final TextView subject){
+        this.unitCache = new UnitCache();
+        this.subject = subject;
+        this.animator = new TextAnimator(subject, 750L, this.animationEndListener);
     }
 
 
     /**
      * Bind the TextView, and the unit into this choreographer object
-     * @param subject The textview to operate on
+     * This method is the entry point for this object
      * @param unit The unit value the TextView is currently at.
+     * @param value Not necessary, as it can be coupled into the Unit, however needed so a client
+     *              doesn't forget to include the value
      */
-    public void bind(@NonNull final TextView subject, @NonNull final Unit unit){
-        animator = new TextAnimator(subject, 750L, this.animationEndListener);
-        unitTracker.update(subject.getId(), unit);
+    public void bind(@NonNull final Unit unit, final double value){
+        updateUnitCache(unit);
+        updateValue(unit, value);
     }
 
 
@@ -40,28 +49,37 @@ public class UnitChoreographer {
      * @param to the new unit type
      */
     public void changeUnit(@NonNull final Unit to){
+
+        if(unitCache.getUnit() == null)
+            throw new IllegalStateException("Failure to call bind() before change unit");
+
+
+        Log.i(TAG, "FROM TYPE: " + unitCache.getUnit().getType());
+        Log.i(TAG, "TO TYPE: " + to.getType());
+
         //Fetch conversion value from la converter
         //Pass to the updateValue method
         //then append the Units type display to it (F, C, mph, kph, K, Kts, etc)
-        updateValue(to, UnitConvertorFactory.convert(unitTracker.getUnit(), to));
+        updateValue(to, UnitConvertorFactory.convert(unitCache.getUnit(), to));
     }
 
 
     /**
      * Changes the  Value of the data, whilst preserving the same Unit type
      * @param toValue The new value (assuming the same unit type)
-     */
-    public void updateValue(@NonNull final double toValue){
-        updateValue(unitTracker.getUnit(), toValue);
-    }
 
+         public void updateValue(@NonNull final double toValue){
+             updateValue(unitCache.getUnit(), toValue);
+         }
+
+     **/
 
     /**
      * Returns the Current Unit the data is at
      * @return The current Tracked unit
      */
     public Unit getCurrentUnit(){
-        return unitTracker.getUnit();
+        return unitCache.getUnit();
     }
 
     /**
@@ -74,18 +92,22 @@ public class UnitChoreographer {
 
 
     private void updateValue(@NonNull final Unit to, final double value){
-        animator.changeText(String.valueOf(value) + to.getType());
+        Log.i(TAG, "value for updateValue: " + String.valueOf(value));
+
+        //Assigning the next unit value so we can add it to the cache.
+        tempTo = to;
+        tempTo.setValue(value);
+        animator.changeText(String.valueOf((int)value) + to.getStringRepresentation());
     }
 
-    private void updateWatcher(@NonNull final Unit to){
-        unitTracker.update(subject.getId(), to);
+    private void updateUnitCache(@NonNull final Unit to){
+        unitCache.update(subject.getId(), to);
     }
 
     private OnAnimationEnd animationEndListener = new OnAnimationEnd() {
         @Override
         public void onAnimationEnd(Animator animation) {
-            updateWatcher(tempTo);
-            tempTo = null;
+            updateUnitCache(tempTo);
         }
     };
 
