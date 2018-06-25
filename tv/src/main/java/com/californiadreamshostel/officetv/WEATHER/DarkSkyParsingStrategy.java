c887e4eff9f355a5147b365f2078bfe9d6b3b0ce
bryan <bryanmills86@yahoo.com>
2018-06-25 09:54:26 -0700

    Implement IParsingStrategy to parse DarkSky.net

---
+++ tv/src/main/java/com/californiadreamshostel/officetv/WEATHER/DarkSkyParsingStrategy.java
@@ -0,0 +1,37 @@
+package com.californiadreamshostel.officetv.WEATHER;
+
+import android.support.annotation.NonNull;
+
+import com.californiadreamshostel.officetv.PARSING.IParsingObserver;
+import com.californiadreamshostel.officetv.PARSING.AbstractIParsingStrategy;
+import com.californiadreamshostel.officetv.NETWORKING.ContentType;
+import com.californiadreamshostel.officetv.WEATHER.MODELS.DARKSKY.DarkSkyWeather;
+import com.google.gson.Gson;
+
+public class DarkSkyParsingStrategy extends AbstractIParsingStrategy<DarkSkyWeather> {
+
+    public DarkSkyParsingStrategy(){
+
+    }
+
+    public DarkSkyParsingStrategy(@NonNull final IParsingObserver observer){
+        super(observer);
+    }
+
+    /**
+     * Parses the data using GSON if the mimeType is json
+     * @param jsonData the Text type of data to parse
+     */
+    @Override
+    public void parseData(@NonNull String jsonData, @NonNull final String contentType) {
+        if(contentType.equals(ContentType.TYPE_JSON))
+             setData(new Gson().fromJson(jsonData, DarkSkyWeather.class));
+    }
+
+}
+
+
+
+
+
+
