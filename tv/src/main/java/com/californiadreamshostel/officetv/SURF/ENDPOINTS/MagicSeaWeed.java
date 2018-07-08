b3e299e7ec72f65d1f8e8cef00769e907430ad35
bryan <bryanmills86@yahoo.com>
2018-07-08 13:01:12 -0700

    Create surf REST schema classese

---
+++ tv/src/main/java/com/californiadreamshostel/officetv/SURF/ENDPOINTS/MagicSeaWeed.java
@@ -0,0 +1,70 @@
+package com.californiadreamshostel.officetv.SURF.ENDPOINTS;
+
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+import com.californiadreamshostel.officetv.WEATHER.API_KEYS;
+
+public class MagicSeaWeed {
+
+    public static final String SCHEME = "http";
+    public static final String HOST = "magicseaweed.com";
+    public static final String ROOT_PATH = "api";
+    public static final String FORECAST_PATH = "forecast";
+
+    public static final String LOCATION_PATH = "663";
+
+    public static final String FIELDS_QUERY = "fields";
+    public static final String SPOT_QUERY = "spot_id";
+
+    public static final String TIMESTAMP = "localTimestamp";
+    public static final String SWELL_MINIMUM = "swell.minBreakingHeight";
+    public static final String SWELL_MAXIMUM = "swell.maxBreakingHeight";
+    public static final String WINDSPEED = "wind.speed";
+
+    private static String resolveParams(final String... params){
+        if(params == null)
+            return "";
+
+        final StringBuilder builder = new StringBuilder();
+
+        for(int i =0; i < params.length; i++){
+
+            final String param = params[i];
+
+            builder.append(param);
+
+            if(i != params.length-1)
+                builder.append(",");
+
+        }
+
+        return builder.toString();
+    }
+
+
+    public static class UriBuilder{
+
+        public Uri getUri(@Nullable final String... params){
+
+            final Uri.Builder builder = new Uri.Builder();
+
+            builder.scheme(SCHEME)
+                    .authority(HOST)
+                    .appendPath(ROOT_PATH)
+                    .appendPath(API_KEYS.API_KEY_MSW)
+                    .appendPath(FORECAST_PATH)
+                    .appendQueryParameter(SPOT_QUERY, LOCATION_PATH);
+
+            if(params !=null)
+                if(params.length > 0)
+                    builder.appendQueryParameter(FIELDS_QUERY, resolveParams(params));
+
+            return builder.build();
+        }
+
+
+    }
+
+
+}
---
+++ tv/src/main/java/com/californiadreamshostel/officetv/SURF/ENDPOINTS/Spitcast.java
@@ -0,0 +1,37 @@
+package com.californiadreamshostel.officetv.SURF.ENDPOINTS;
+
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+public class Spitcast {
+
+    public static final String SCHEME = "http";
+    public static final String HOST = "api.spitcast.com";
+    public static final String PATH_API = "api";
+    public static final String PATH_COUNTY = "county";
+
+    public static final String PATH_COUNTY_NAME = "san-diego";
+
+    //Spit cast Tide Query parameter
+    public static final String PATH_TIDE = "tide";
+
+    public static class UriBuilder{
+
+        public Uri getUri(@NonNull final String dataType){
+
+            final Uri.Builder b = new Uri.Builder();
+
+            b.scheme(SCHEME)
+             .authority(HOST)
+             .appendPath(PATH_API)
+             .appendPath(PATH_COUNTY)
+             .appendPath(dataType)
+             .appendPath(PATH_COUNTY_NAME);
+
+        return b.build();
+        }
+
+
+    }
+
+}
