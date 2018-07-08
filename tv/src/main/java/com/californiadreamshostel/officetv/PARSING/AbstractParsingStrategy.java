c70ef152cefb57f11f64342a9891778e2522aa67
bryan <bryanmills86@yahoo.com>
2018-07-08 12:58:47 -0700

    Make base parsing classes

--- tv/src/main/java/com/californiadreamshostel/officetv/PARSING/AbstractParsingStrategy.java
+++ tv/src/main/java/com/californiadreamshostel/officetv/PARSING/AbstractParsingStrategy.java
@@ -1,7 +1,14 @@
 package com.californiadreamshostel.officetv.PARSING;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.ResultReceiver;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.californiadreamshostel.officetv.NETWORKING.ContentType;
+import com.californiadreamshostel.officetv.NETWORKING.DOWNLOADING.DataDownloadService;
 
 
 /**
@@ -9,24 +16,41 @@ import android.support.annotation.Nullable;
  * Subclasses are responsible for setting the generic type of this class
  * @param <T> The object type we're working with
  */
-public abstract class AbstractParsingStrategy<T> implements IParsingStrategy {
+public abstract class AbstractParsingStrategy<T> extends ResultReceiver{
 
     private T data;
-    private IParsingObserver observer;
+    private IParsingFinishedObserver observer;
 
     public AbstractParsingStrategy(){
-
+        super(new Handler());
     }
 
-    public AbstractParsingStrategy(@NonNull final IParsingObserver observer){
+    public AbstractParsingStrategy(@NonNull final IParsingFinishedObserver observer){
+        this();
+
         this.observer = observer;
     }
 
-    public abstract void parseData(@NonNull String response, @NonNull String contentType);
+    @Override
+    protected void onReceiveResult(int resultCode, Bundle resultData) {
 
-    public void setObserver(@NonNull IParsingObserver IParsingObserver) {
-        this.observer = IParsingObserver;
+        Log.i("WEATHER_SURF_JOB", "RESULT CODE: " + resultCode);
+        Log.i("WEATHER_SURF_JOB", "RESPONSE: " + resultData.getString(DataDownloadService.DATA_KEY));
+
+        if(resultCode == DataDownloadService.SUCCESS){
+
+            final String response = resultData.getString(DataDownloadService.DATA_KEY);
+
+            if(!response.equals(DataDownloadService.NO_DATA))
+                parseData(response, ContentType.TYPE_JSON);
+            else
+                Log.i("WEATHER_SURF_JOB", "The data returned as NO_DATA");
+        }
+
     }
+
+
+    protected abstract void parseData(@NonNull String response, @NonNull String contentType);
 
     protected void setData(@Nullable final T t){
         this.data = t;
@@ -37,6 +61,7 @@ public abstract class AbstractParsingStrategy<T> implements IParsingStrategy {
     protected void notifyObserver(){
         if(observer != null)
             observer.onDataParsed(getData());
+
     }
 
     @Nullable
