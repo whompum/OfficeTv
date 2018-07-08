8c7ba678ba3bc7e752ebc29ac34984a29e9e954c
bryan <bryanmills86@yahoo.com>
2018-07-08 13:08:59 -0700

    Remove obsolete networking models, and business logic

--- tv/src/main/java/com/californiadreamshostel/officetv/SURF/HighTideAdaptable.java
+++
@@ -1,9 +0,0 @@
-package com.californiadreamshostel.officetv.SURF;
-
-import com.californiadreamshostel.officetv.SURF.MODELS.HighTide;
-
-public class HighTideAdaptable<T extends HighTide> {
-
-
-
-}
